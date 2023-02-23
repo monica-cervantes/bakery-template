2bff3e927f4f1da519f75ef074c9e2dcc4e4f111
monica-cervantes <46661858+monica-cervantes@users.noreply.github.com>
2023-02-23 20:29:44 +0800

    added homepage sections

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 $(window).on('load', function() {
   $("#loader-wrapper").fadeOut(700);
-  console.log("hi!");
+
+  var thumbsSwiper = new Swiper(".ThumbsSwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    spaceBetween: 10,
+    slidesPerView: 3,
+    watchSlidesProgress: true,
+  });
+  
+  var mainSwiper = new Swiper(".MainSwiper", {
+    spaceBetween: 10,
+    thumbs: {
+      swiper: thumbsSwiper,
+    },
+  });
+
+  $(window).on('scroll', function() {
+    if($(window).scrollTop() > 50) {
+      $('header').addClass('scrolled');
+    } else {
+      $('header').removeClass('scrolled');
+    }
+  });
 });
 
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
+
